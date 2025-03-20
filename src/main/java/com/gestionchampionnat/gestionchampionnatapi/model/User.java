46f6ae872a4a813b5bd67fd01e6d16c23f5ec587
Berthet <lu.berthet@gmail.com>
2025-03-20 09:39:33 +0100

    first commit

---
+++ src/main/java/com/gestionchampionnat/gestionchampionnatapi/model/User.java
@@ -0,0 +1,28 @@
+package com.gestionchampionnat.gestionchampionnatapi.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
+    private LocalDate creationDate;
+
+}
